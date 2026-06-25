c49d42c3a65a492c488ccfd6696596576c2bce7c
gk.fabi <facortezgo@gmail.com>
2026-06-25 16:44:21 -0500

    Web server initialization on ESP8266, comfirmation of client connection

--- sensing_node/sensing_node.ino
+++ sensing_node/sensing_node.ino
@@ -9,39 +9,57 @@
 const char* ssid = "ASTRID G";
 const char* wifi_password = "82330693";
 
-//Creación del objeto del sensor DHT e inicialización
+//Creación se objetos
 DHT dht(dht_pin, DHT11);
+WiFiServer server(80);
 
 void setup(){
   Serial.begin(115200);
   delay(500);
-  Serial.println("");
 
   //Configuración de pines e inicialización de DHT11
   pinMode(yl_pin, INPUT);
   pinMode(raindrop_pin, INPUT);
   dht.begin();
-  Serial.println("¡Configuración de pines e inicialización de sensores de manera exitosa!");
+  Serial.println(F("\n¡Configuración de pines e inicialización de sensores de manera exitosa!"));
 
-  //Conexión WiFi
-  Serial.println("-------------------------------------------------------");
-  Serial.print("Conectándose a la red WiFi \"");
-  Serial.print(ssid);
-  Serial.print("\"");
+  //Conexión WiFi e inicialización del server
+  Serial.printf("Conectándose a la red WiFi \"%s\"", ssid);
   WiFi.begin(ssid, wifi_password);
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(500);
   }
+  Serial.print(F("\n¡Conexión exitosa! Dirección IP del servidor: "));
+  Serial.print(WiFi.localIP());
   Serial.println("");
-  Serial.println("¡Conexión exitosa!");
-  Serial.print("Dirección IP del NodeMCU: ");
-  Serial.println(WiFi.localIP());
-  Serial.println("-------------------------------------------------------");
-  Serial.println("");
+  server.begin();
 }
 
 void loop(){
+  //Web server request
+  WiFiClient client = server.available();
+  if(client){
+    Serial.println(F("Cliente conectado al servidor web\n"));
+    while(client.connected()){
+      if(client.available()){
+        String line = client.readStringUntil('\r'); //Lee línea por línea la petición del cliente
+        Serial.print(line);
+        if(line.length() == 1 && line[0] == '\n'){ //La finalización de la petición del cliente se marca con un salto de línea vacío
+          //Página web a entregar
+          //...
+          //...
+        break;
+        }
+      }
+      //Se revisa si aún se reciben bytes del encabezado TCP de la petición del cliente, para asegurarse de no cortar la conexión muy pronto
+      while(client.available()){
+        client.read();
+      }
+      client.stop();
+    }
+  }
+
   //Variables que albergan datos leídos
   float air_humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
